b49b188bc0df4f7266d8b32c58102250efd07169
Tim Wanders <timwanders241@gmail.com>
2018-10-22 20:13:11 +0200

    Changed project name

--- Source/RimWorldTool/Utilities/Utils.cs
+++ Source/RimWorldTool/Utilities/Utils.cs
@@ -1,5 +1,5 @@
 /*
- RimWorldCli - https://gitlab.com/tim241/RimWorldCli
+ RimWorldTool - https://gitlab.com/tim241/RimWorldTool
  Copyright (C) 2018 Tim Wanders <tim241@mailbox.org>
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
